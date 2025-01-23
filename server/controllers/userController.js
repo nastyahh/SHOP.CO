59a2f29f3e48111a4cf13314544088826ec2e145
nastyahh <nasstyahus@gmail.com>
2025-01-23 17:30:21 +0300

    switch errors to english

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,25 +3,25 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { User, Cart } = require('../models/models')
 
-const generateJwt = (id, email, role) => {
-    return jwt.sign({ id, email, role }, process.env.SECRET_KEY, { expiresIn: '24h' })
+const generateJwt = (id, username, email, role) => {
+    return jwt.sign({ id, username, email, role }, process.env.SECRET_KEY, { expiresIn: '1m' })
 }
 
 class UserController {
     async registration(req, res, next) {
-        const { email, password, role } = req.body
+        const { username, email, password, role } = req.body
         if (!email || !password) {
-            return next(ApiError.badRequest('Некорректный email или пароль'))
+            return next(ApiError.badRequest('Incorrect email or password'))
         }
         const candidate = await User.findOne({ where: { email } })
         if (candidate) {
-            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+            return next(ApiError.badRequest('User with such email already exists'))
         }
         const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({ email, password: hashPassword, role })
+        const user = await User.create({ username, email, password: hashPassword, role })
         const cart = await Cart.create({ userId: user.id })
 
-        const token = generateJwt(user.id, user.email, user.role)
+        const token = generateJwt(user.id, user.username, user.email, user.role)
 
         return res.json({ token })
     }
@@ -30,19 +30,20 @@ class UserController {
         const { email, password } = req.body
         const user = await User.findOne({ where: { email } })
         if (!user) {
-            return next(ApiError.internal('Пользователь с таким email не найден'))
+            return next(ApiError.internal('User with such email not found'))
         }
         let comparePassword = bcrypt.compareSync(password, user.password)
         if (!comparePassword) {
-            return next(ApiError.internal('Неверный пароль'))
+            return next(ApiError.internal('Incorrect password'))
         }
-        const token = generateJwt(user.id, user.email, user.role)
+        console.log("login user", user)
+        const token = generateJwt(user.id, user.username, user.email, user.role)
 
         return res.json({ token })
     }
 
     async checkAuth(req, res, next) {
-        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        const token = generateJwt(req.user.id, req.user.username, req.user.email, req.user.role)
         return res.json({ token })
     }
 }
