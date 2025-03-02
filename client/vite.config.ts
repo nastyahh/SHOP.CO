73c7135e70f8d695b58c66accffb67a86b6200c5
nastyahh <nasstyahus@gmail.com>
2025-03-02 16:36:54 +0300

    change paths config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -15,5 +16,10 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   base: "./",
 });
